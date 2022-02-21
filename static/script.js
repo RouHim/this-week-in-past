69765e210f3e1d75a661eedda6102b62d7b8201d
Rouven Himmelstein <rouven@himmelstein.info>
2022-02-21 18:59:19 +0100

    feat: home assistant weather

--- static/script.js
+++ static/script.js
@@ -5,18 +5,40 @@ let maxIndex = 0;
 window.onload = () => {
     loadAvailableImages();
     loadCurrentWeather();
+    loadCurrentTempFromHomeAssistant();
 };
 
 function loadCurrentWeather() {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Koblenz&appid=4021b60be2b322c8cfc749a6503bb553&units=metric&lang=de';
+    const city = 'Koblenz';
+    const app_id = '4021b60be2b322c8cfc749a6503bb553';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${app_id}&units=metric&lang=de`;
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
             const weather = data.weather[0];
-            const temp = data.main.temp;
             const icon = weather.icon;
-            document.getElementById("weather-temp").innerHTML = weather.description + ", " + Math.round(temp) + "°C";
-            document.getElementById("weather-icon").src = "https://openweathermap.org/img/w/" + icon + ".png";
+            document.getElementById("weather-label").innerHTML = weather.description;
+            document.getElementById("weather-icon").src = `https://openweathermap.org/img/w/${icon}.png`;
+        });
+}
+
+function loadCurrentTempFromHomeAssistant() {
+    const base_url = "http://192.168.0.15:8123";
+    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIzYzg5ZmY3MjMyZDg0ZmY2ODVkZDFkODhhOWQxYTRjMiIsImlhdCI6MTY0NTQ1OTA2MiwiZXhwIjoxOTYwODE5MDYyfQ.TknsqBMwriiE4_jrSjEi4z8vn0AUvLD8WYgL-BhaYKw";
+    const entity_id = "sensor.0x00158d0006ec850a_temperature";
+    const url = `${base_url}/api/states/${entity_id}`;
+
+    fetch(url, {
+        "method": "GET",
+        "headers": {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("weather-temperature").innerText = Math.round(data.state) + "°C";
         });
 }
 
