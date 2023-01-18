d94a9257f84100e78888eb7337e626c5e3cdff84
Rouven Himmelstein <rouven@himmelstein.info>
2023-01-18 13:14:15 +0100

    fix(pipeline): rust target triple conversion

--- translate-arch-to-rust-tripple.sh
+++ translate-arch-to-rust-tripple.sh
@@ -5,20 +5,27 @@
 #
 # # # #
 
-CURRENT_ARCH=$(uname -m)
+# Get input string
+input_string=$1
 
-if [ "$CURRENT_ARCH" = "armv7l" ]; then
-  CURRENT_ARCH="armv7"
+# Define a function to convert the input string to a rust target triple
+function to_triple() {
+  case $1 in
+    "x86_64-musl") echo "x86_64-unknown-linux-musl";;
+    "aarch64-musl") echo "aarch64-unknown-linux-musl";;
+    "armv7-musleabihf") echo "armv7-unknown-linux-musleabihf";;
+    "arm-musleabihf") echo "arm-unknown-linux-musleabihf";;
+    *) echo "Error: Unsupported input string" && return 1;;
+  esac
+}
+
+# Call the function and save the output to a variable
+output=$(to_triple "$input_string")
+
+# check if the function returned an error
+if [ $? -eq 1 ]; then
+  exit 1
 fi
 
-# Iterate all folders in the target folder
-for arch_folder in $(find target -maxdepth 1 -type d); do
-    # Check if the folder name contains the current arch
-    if [[ "$arch_folder" = *"$CURRENT_ARCH"* ]]; then
-      RUST_TRIPLE=$(echo "$arch_folder" | sed -e "s/.*\///g")
-      echo "$RUST_TRIPLE"
-      exit 0
-    fi
-done
-
-exit 1
+# Print the output
+echo "$output"
